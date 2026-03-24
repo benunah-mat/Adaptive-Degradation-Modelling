9b77df578e718b839cb9f7a540872532e2d0fa58
benunah-mat <benunah@gmail.com>
2026-03-24 17:20:12 +0000

    run_digital_twin_framework.m

--- run_digital_twin_framework.m
+++ run_digital_twin_framework.m
@@ -18,3 +18,33 @@ machines = digital_twin_simulatorx(num_machines,t,dt);
 fleet_failure_ranking(machines)
 
 fprintf('\nSimulation Complete.\n')
+
+%% ============================================================
+% PROBABILISTIC RUL ANALYSIS
+% ============================================================
+
+machine = machines(1);   % select machine
+
+num_sim = 100;
+
+k = 1e-4;
+a = 1.5;
+b = 1.2;
+c = 1.1;
+d = 1.3;
+
+failure_times = monte_carlo_rul(machine,t,dt,k,a,b,c,d,num_sim);
+
+figure
+histogram(failure_times,20)
+xlabel('Failure Time')
+ylabel('Frequency')
+title('Failure Time Distribution')
+grid on
+
+mean_tf = mean(failure_times);
+std_tf = std(failure_times);
+
+fprintf("Mean Failure Time: %.2f\n",mean_tf);
+fprintf("Std Dev: %.2f\n",std_tf);
+
