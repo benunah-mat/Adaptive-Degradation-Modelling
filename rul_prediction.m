1dd624dfaa3de904620fd70e5826de86e42d8f45
benunah-mat <benunah@gmail.com>
2026-04-10 02:48:34 +0100

    rul_prediction.m

--- rul_prediction.m
+++ rul_prediction.m
@@ -1,16 +1,7 @@
-function RUL = rul_prediction(failure_index,length_signal)
+function RUL = rul_prediction(failure_index, length_signal)
 
-RUL = zeros(1,length_signal);
+RUL = zeros(1, length_signal);
 
 for i = 1:length_signal
-
-RUL(i) = failure_index - i;
-
+    RUL(i) = failure_index - i;
 end
-
-figure
-plot(RUL,'LineWidth',2)
-xlabel('Time Window')
-ylabel('Remaining Useful Life')
-title('Remaining Useful Life Prediction')
-grid on
